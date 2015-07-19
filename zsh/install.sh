572c6073edf1220f5c64a8f84ffc6b05fe841062
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2015-07-19 03:36:30 +0200

    Added own prezto theme

--- zsh/install.sh
+++ zsh/install.sh
@@ -3,3 +3,6 @@
 for file in $(ls .z* 2> /dev/null); do
   ln -s "$PWD/$file" "$HOME/$file"
 done
+
+theme_name="prompt_herman_setup"
+ln -s "$PWD/$theme_name" "$HOME/.zprezto/modules/prompt/functions/$theme_name"
